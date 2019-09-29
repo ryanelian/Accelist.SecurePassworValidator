bb055bc776757d11b550c459c3725a7d997a7f0e
Ryan Elian <ryan.elian.cs@gmail.com>
2019-09-29 09:00:06 +0700

    Add password distiller project.

---
+++ Accelist.SecurePasswordValidator.PasswordDistiller/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Accelist.SecurePasswordValidator.PasswordDistiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 0;
+
+            using (var dump = new FileStream("rockyou-3min9.txt", FileMode.Create))
+            using (var file = new FileStream("rockyou-withcount.txt", FileMode.Open))
+            using (var sw = new StreamWriter(dump))
+            using (var sr = new StreamReader(file))
+            {
+                string s = sr.ReadLine();
+                while (s != null)
+                {
+                    var countString = s.Substring(0, 8);
+                    var password = s.Substring(8);
+                    var count = int.Parse(countString.Trim());
+                    if (count < 3)
+                    {
+                        break;
+                    }
+
+                    //Console.WriteLine($"{count}:\"{password}\"");
+                    //Console.ReadLine();
+                    if (password.Length >= 9)
+                    {
+                        sw.WriteLine(password);
+                        n++;
+                    }
+
+                    s = sr.ReadLine();
+                }
+            }
+
+        }
+    }
+}
